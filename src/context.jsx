835802fa472c88b40bec563548252d0e9b415de9
Eduard Dinu <dinueduard22121999@gmail.com>
2024-01-01 17:14:57 +0100

    first commit

---
+++ src/context.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState, useEffect } from "react";
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("alien");
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    document.body.classList.toggle("dark-theme", newDarkTheme);
+  };
+  return (
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
